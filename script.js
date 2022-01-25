352553584c0b4af3451f9077f8bcc9df8b317551
priyanshu kumar keshri <priyanshukumarkeshri6@gmail.com>
2022-01-25 20:48:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-const textarea = document.querySelector("textarea");
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
 speechBtn = document.querySelector("button");
-voiceList = document.querySelector("select");
 
-let synth = speechSynthesis;
+let synth = speechSynthesis,
 isSpeaking = true;
 
 voices();
 
 function voices(){
     for(let voice of synth.getVoices()){
-        // selecting "Google US English" voice as default
         let selected = voice.name === "Google US English" ? "selected" : "";
-        //Creating an option tag with passing voice name and voice language
-        let option = `<option value="${voice.name}"${selected}>${voice.name} (${voice.lang}) </option>`;
-        voiceList.insertAdjacentHTML("beforeend",option);//inserting option tag beforeend 
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
     }
 }
 
 synth.addEventListener("voiceschanged", voices);
 
 function textToSpeech(text){
-    let utternance = new SpeechSynthesisUtterance(text);
+    let utterance = new SpeechSynthesisUtterance(text);
     for(let voice of synth.getVoices()){
-        // if the available device voice ame is equal to the user selecetd voice name
-        // then set the speech voice to the user selected voice
         if(voice.name === voiceList.value){
-            utternance.voice = voice;
+            utterance.voice = voice;
         }
     }
-    speechSynthesis.speak(utternance); // speak the speech/utternance
+    synth.speak(utterance);
 }
 
-speechBtn.addEventListener("click",e => {
+speechBtn.addEventListener("click", e =>{
     e.preventDefault();
     if(textarea.value !== ""){
-        if(!synth.speaking){// if an utternance / speech is not currently in the process of speaking
-        textToSpeech(textarea.value);
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
         }
-        if(textarea.value.length>80){
-            // if isSpeaking is true then change it's value to false and resume the uternance
-            //else change it's value to true and pause the speech
+        if(textarea.value.length > 80){
+            setInterval(()=>{
+                if(!synth.speaking && !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convert To Speech";
+                }else{
+                }
+            }, 500);
             if(isSpeaking){
                 synth.resume();
                 isSpeaking = false;
@@ -49,13 +50,6 @@ speechBtn.addEventListener("click",e => {
                 isSpeaking = true;
                 speechBtn.innerText = "Resume Speech";
             }
-
-            setInterval(() => {
-                if(!synth.speaking && !isSpeaking){
-                    isSpeaking = true ;
-                    speechBtn.innerText = "Convert To Speech";
-                }
-            });
         }else{
             speechBtn.innerText = "Convert To Speech";
         }
